3053afda6a60451d67f3e1ef5f9539c905c3eec6
Aleksey Nogay <leshkanogay@gmail.com>
2023-10-10 20:13:00 +0500

    comented js

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-let num1 = +prompt('Введите первое число')
-let num2 = +prompt('Введите второе число')
+// let num1 = +prompt('Введите первое число')
+// let num2 = +prompt('Введите второе число')
 
 // let ans = alert(num1 + num2) // нахождение суммы
-let avg = alert((num1 + num2)/2) // среднее арифметическое
+// let avg = alert((num1 + num2)/2) // среднее арифметическое
 // let sqrt = alert((num1**2 + num2**2)/2) //среднее арифметическое их квадратов
 
 
 
 //    calculator
 
-let btn = document.querySelector('.Btn')
+// let btn = document.querySelector('.Btn')
 
-btn.addEventListener('click', calc)
+// btn.addEventListener('click', calc)
 
-function calc(){
-    let num1 = document.querySelectorAll('.number')[0].value
-    let num2 = document.querySelectorAll('.number')[1].value
-    let sign = document.querySelector('.sign').value
-    let result = document.querySelector('.result')
-    if(sign === '+'){
-        result.innerHTML = Number(num1) + Number(num2)
-    }else if(sign === '-'){
-        result.innerHTML = Number(num1) - Number(num2)
-    }else if(sign === '*'){
-        result.innerHTML = Number(num1) * Number(num2)
-    }else if(sign === '/'){
-        result.innerHTML = Number(num1) / Number(num2)
-    }
-    return;
+// function calc(){
+//     let num1 = document.querySelectorAll('.number')[0].value
+//     let num2 = document.querySelectorAll('.number')[1].value
+//     let sign = document.querySelector('.sign').value
+//     let result = document.querySelector('.result')
+//     if(sign === '+'){
+//         result.innerHTML = Number(num1) + Number(num2)
+//     }else if(sign === '-'){
+//         result.innerHTML = Number(num1) - Number(num2)
+//     }else if(sign === '*'){
+//         result.innerHTML = Number(num1) * Number(num2)
+//     }else if(sign === '/'){
+//         result.innerHTML = Number(num1) / Number(num2)
+//     }
+//     return;
     
-}
+// }
 
